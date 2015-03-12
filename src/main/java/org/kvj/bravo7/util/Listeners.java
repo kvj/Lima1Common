3de5f5de14d6d2938483554b15fa0c9e416f7d15
Konstantin Vorobyev <kvorobyev@gmail.com>
2015-03-13 07:40:29 +0900

    Listeners made easy

---
+++ src/main/java/org/kvj/bravo7/util/Listeners.java
@@ -0,0 +1,59 @@
+package org.kvj.bravo7.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Helper class for organizing listeners to events
+ * Created by kvorobyev on 3/13/15.
+ */
+public class Listeners<T> {
+
+    public static interface ListenerEmitter<T> {
+        public boolean emit(T listener);
+    }
+
+    private List<T> listeners = new ArrayList<>();
+    private final Object lock = new Object();
+
+    public boolean add(T listener) {
+        synchronized (lock) {
+            if (listeners.contains(listener)) { // Already there
+                return false;
+            }
+            listeners.add(listener);
+            return true;
+        }
+    }
+
+    public boolean remove(T listener) {
+        synchronized (lock) {
+            if (!listeners.contains(listener)) { // There
+                return false;
+            }
+            listeners.remove(listener);
+            return true;
+        }
+    }
+
+    public boolean clear() {
+        synchronized (lock) {
+            if (!listeners.isEmpty()) {
+                listeners.clear();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean emit(ListenerEmitter<T> emitter) {
+        synchronized (lock) {
+            for (T listener : listeners) { // $COMMENT
+                if (!emitter.emit(listener)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
