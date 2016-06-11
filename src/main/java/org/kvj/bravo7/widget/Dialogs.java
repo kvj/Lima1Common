f19b87da99032cf34e20acfce4e780c0bacef100
Konstantin Vorobyev <kvorobyev@gmail.com>
2016-06-11 21:42:11 +0900

    Use buttons in question dialog

--- src/main/java/org/kvj/bravo7/widget/Dialogs.java
+++ src/main/java/org/kvj/bravo7/widget/Dialogs.java
@@ -3,7 +3,11 @@ package org.kvj.bravo7.widget;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 /**
@@ -43,6 +47,8 @@ public class Dialogs {
         AlertDialog.Builder builder = new AlertDialog.Builder(context)
             .setTitle(title)
             .setMessage(message);
+        final AlertDialog dialog;
+        Button[] items = new Button[captions.length];
         if (captions.length == 0) {
             // OK/Cancel
             DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
@@ -56,20 +62,33 @@ public class Dialogs {
                 .setPositiveButton("OK", listener)
                 .setNegativeButton("Cancel", listener);
         } else {
-            CharSequence[] items = new CharSequence[captions.length];
-            for (int i = 0; i < captions.length; i++) { // $COMMENT
-                items[i] = captions[i];
+            LinearLayout ll = new LinearLayout(context);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams llp =
+                    new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+
+            for (int i = 0; i < captions.length; i++) { // Create buttons
+                Button b = new AppCompatButton(context);
+                b.setText(captions[i]);
+                items[i] = b;
+                ll.addView(b, llp);
             }
-            DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                    callback.run(which);
-                }
-            };
-            builder.setItems(items, listener);
+            builder.setView(ll);
         }
-        return builder.show();
+        dialog = builder.show();
+        for (int i = 0; i < items.length; i++) { // $COMMENT
+            final int index = i;
+            items[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                    callback.run(index);
+                }
+            });
+        }
+        return dialog;
     }
 
     public static void toast(Context context, String message) {
